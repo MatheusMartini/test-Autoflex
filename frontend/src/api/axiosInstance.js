508531388dcf024d267e153c4075107b70450bfe
MatheusMartini <matheus.martini@rede.ulbra.br>
2026-02-25 18:04:02 -0300

    change baseURL to /api

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080', // porta Quarkus
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
