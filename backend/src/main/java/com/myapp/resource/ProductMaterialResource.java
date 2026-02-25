587568560ff46727832f54202644d822d06c4fca
MatheusMartini <matheus.martini@rede.ulbra.br>
2026-02-25 20:18:37 -0300

    updated methods

--- backend/src/main/java/com/myapp/resource/ProductMaterialResource.java
+++ backend/src/main/java/com/myapp/resource/ProductMaterialResource.java
@@ -1,12 +1,14 @@
 package com.myapp.resource;
 
 import com.myapp.dto.ProductMaterialRequestDTO;
+import com.myapp.dto.ProductMaterialResponseDTO;
 import com.myapp.service.ProductMaterialService;
+
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
-
+import java.util.List;
 @Path("/product-materials")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
@@ -16,7 +18,31 @@ public class ProductMaterialResource {
     ProductMaterialService service;
 
     @POST
-    public void create(ProductMaterialRequestDTO dto) {
-        service.create(dto);
+    public ProductMaterialResponseDTO create(ProductMaterialRequestDTO dto) {
+        return service.create(dto);
+    }
+
+    @PUT
+    @Path("/{id}")
+    public ProductMaterialResponseDTO update(@PathParam("id") Long id,
+    ProductMaterialRequestDTO dto) {
+        return service.update(id, dto);
+    }
+
+    @GET
+    public List<ProductMaterialResponseDTO> list() {
+        return service.listAll();
+    }
+
+    @GET
+    @Path("/{id}")
+    public ProductMaterialResponseDTO findById(@PathParam("id") Long id) {
+        return service.findById(id);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public void delete(@PathParam("id") Long id) {
+        service.delete(id);
     }
 }
